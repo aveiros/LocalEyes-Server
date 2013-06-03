b197b63134b49f62486ab728f5e618e368953b70
aveiros <aqviveiros@gmail.com>
2013-06-03 19:01:57 +0100

    removed database/resources package

--- src/com/lisbonbigapps/myhoster/database/resources/ServiceResourceFactory.java
+++
@@ -1,6 +0,0 @@
-package com.lisbonbigapps.myhoster.database.resources;
-
-import com.lisbonbigapps.myhoster.database.entities.EntityService;
-
-public class ServiceResourceFactory extends GenericResourceFactory<EntityService> {
-}
