85cb113301ca50ea46ebd00a2db3bcd3cbb50317
Aquilino Viveiros <aqviveiros@gmail.com>
2014-05-20 08:53:02 +0100

    data cleanup

--- src/com/lisbonbigapps/myhoster/database/Bootstrap.java
+++ src/com/lisbonbigapps/myhoster/database/Bootstrap.java
@@ -13,20 +13,20 @@ public class Bootstrap {
 	user1.setLongitude(0);
 	user1.setLatitude(0);
 	user1.setHosting(true);
-	user1.setPhoneNumber("+351966455966");
+	user1.setPhoneNumber("+351960011222");
 
 	EntityServiceProfile profile1 = new EntityServiceProfile();
 	profile1.setUser(user1);
 	profile1.setFee(20);
 
 	EntityUser user2 = new EntityUser();
-	user2.setName("Cláudio Teixeira");
+	user2.setName("Clï¿½udio Teixeira");
 	user2.setUsername("claudiotx");
 	user2.setPassword("12345");
 	user2.setLongitude(0);
 	user2.setLatitude(0);
 	user2.setHosting(true);
-	user2.setPhoneNumber("+351968648616");
+	user2.setPhoneNumber("+351961122333");
 
 	EntityServiceProfile profile2 = new EntityServiceProfile();
 	profile2.setUser(user2);
@@ -39,7 +39,7 @@ public class Bootstrap {
 	user3.setLongitude(0);
 	user3.setLatitude(0);
 	user3.setHosting(true);
-	user3.setPhoneNumber("+351968277447");
+	user3.setPhoneNumber("+351962233444");
 
 	EntityServiceProfile profile3 = new EntityServiceProfile();
 	profile3.setUser(user3);
@@ -52,7 +52,7 @@ public class Bootstrap {
 	user4.setLongitude(0);
 	user4.setLatitude(0);
 	user4.setHosting(true);
-	user4.setPhoneNumber("+351900000000");
+	user4.setPhoneNumber("+351963344555");
 
 	EntityServiceProfile profile4 = new EntityServiceProfile();
 	profile4.setUser(user4);
